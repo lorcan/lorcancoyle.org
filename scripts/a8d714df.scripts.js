c093700f8995d76ade2e414186f1e5b07f28dea5
Lorcan Coyle <lorcan@gmail.com>
2013-04-27 23:36:02 +0100

    Added the dist directory
    Testing a second deployment.

---
+++ scripts/a8d714df.scripts.js
@@ -0,0 +1,1 @@
+define([],function(){"use strict";return"'Allo 'Allo!"}),angular.module("lorcancoyle.orgApp").controller("BookCtrl",["$scope","$http","$log",function(o,t,n){"use strict";t.get("/scripts/books.json").then(function(t){o.books=t.data}),n.info("BookCtrl initialisation completed.")}]);
