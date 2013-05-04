837137ec4ef774f211bd8ead6ad1d3f610c5f7f1
Lorcan Coyle <lorcan@gmail.com>
2013-05-04 22:14:57 +0100

    Renamed the app

--- app/scripts/controllers/BookCtrl.js
+++ app/scripts/controllers/BookCtrl.js
@@ -1,4 +1,4 @@
-angular.module('lorcancoyle.orgApp').controller('BookCtrl', function($scope, $http, $log) {
+angular.module('lorcancoyleorg').controller('BookCtrl', function($scope, $http, $log) {
    'use strict';
 
    $http.get('/scripts/books.json').then(function(res) {
