f89d9e01906f7c8fd80be1a86507d36c38120e93
Lorcan Coyle <lorcan@gmail.com>
2013-05-07 09:36:41 +0100

    JSHint should not check the vendor scripts.

--- Gruntfile.js
+++ Gruntfile.js
@@ -95,7 +95,8 @@ module.exports = function (grunt) {
       },
       all: [
         'Gruntfile.js',
-        '<%= yeoman.app %>/scripts/{,*/}*.js'
+        '<%= yeoman.app %>/scripts/*.js',
+        '<%= yeoman.app %>/scripts/controllers/*.js'
       ]
     },
     karma: {
